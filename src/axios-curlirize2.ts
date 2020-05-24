8db2416c54dd952955cb49b7d12e4bff83326a29
Vlad <vlad.open.source@gmail.com>
2020-05-24 15:37:06 +0300

    Update axios-curlirize2.ts

--- src/axios-curlirize2.ts
+++ src/axios-curlirize2.ts
@@ -1,3 +1,18 @@
 export default () => {
 	console.log("123");
+	console.log("123");
+	console.log("123");
+	console.log("123");
+	console.log("123");
+	console.log("123");
+	console.log("123");
+	console.log("123");
+	console.log("123");
+	console.log("123");
+	console.log("123");
+	console.log("123");
+	console.log("123");
+	console.log("123");
+	console.log("123");
+	console.log("123");
 };
